80ef686959e58697398eab05f08b9529c5be77ef
Ryan Dale <rd01748@georgiasouthern.edu>
2014-03-25 17:11:55 -0400

    Completed Extra Credit #14 by adding a datetime field to each model and setting it to the date created. Also modified the houses template to show the time there as well.

--- atlatl/atlatltest/models.py
+++ atlatl/atlatltest/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+import datetime
 
 class Owner(models.Model):
 	name=models.TextField()
+	date_created=models.DateTimeField(default=datetime.datetime.now())
 
 	def __unicode__(self):
 		return u'%s' %(self.name)
@@ -9,6 +11,7 @@ class Owner(models.Model):
 class House(models.Model):
     address=models.TextField()
     owner=models.ForeignKey(Owner,related_name="owner")
-    
+    date_created=models.DateTimeField(default=datetime.datetime.now())
+
     def __unicode__(self):
 		return u'%s' %(self.address)
