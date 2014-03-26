9df23e64c5d11f983609b15085920dcacb0c9df6
Ryan Dale <rd01748@georgiasouthern.edu>
2014-03-25 21:46:31 -0400

    Completed Extra Credit #1 & #2

---
+++ atlatl/atlatltest/management/commands/delete_house.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand, CommandError
+from optparse import make_option
+from atlatltest.models import Owner, House
+
+class Command(BaseCommand):
+	help="Deletes a house. Also deletes the user if the user doesn't have any other houses."
+	option_list = BaseCommand.option_list + (
+		make_option('--addr-contains',
+		            action='store',
+		            dest='address',
+		            default=False,
+		            help=''),
+		)
+	def handle(self, *args, **options):
+		if options['address']:
+			if House.objects.filter(address__contains=options['address']).exists():
+				house=House.objects.filter(address__contains=options['address'])[0]
+				self.stdout.write("Deleted house: address=[%s] owner=[%s]"%(house.address,house.owner.name))
+				if House.objects.filter(owner=house.owner).count()==1:
+					house.owner.delete()
+				house.delete()
+			
